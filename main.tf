08ee75840109cea55dddf4ee75648b5efa03a25d
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-21 19:26:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,19 +107,3 @@ output "instance_ssh_key" {
   value      = "${abspath(path.root)}/ssh_key"
   depends_on = [tls_private_key.ssh]
 }
-
-
-data "google_dns_managed_zone" "env_dns_zone" {
-  name = "my-cloudrroot7-domain-zone"
-}
-
-
-resource "google_dns_record_set" "default" {
-  name         = "nginx.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
-  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
-  type         = "A"
-  ttl          = 300
-  rrdatas = [
-    google_compute_instance.demo.network_interface[0].access_config[0].nat_ip
-  ]
-}
